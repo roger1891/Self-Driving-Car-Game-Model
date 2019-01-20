8eae90790eb06507fdb9b0578f7814370a7e84c7
DESKTOP-AJ96G0N\laptop\Roger <rogerrainersbraun@gmail.com>
2019-01-21 00:21:18 +0800

    commencing ai class file

---
+++ ai.py
@@ -0,0 +1,14 @@
+# AI for Self Driving Car
+
+# Importing the libraries
+
+import numpy as np #play and work with arrays
+import random # random samples
+import os # load the model
+import torch #it can handle dynamic graphs
+import torch.nn as nn #neural networks
+import torch.nn.functional as F #different functions to be used
+import torch.optim as optim #optimizers to perform categorization
+import torch.autograd as autograd #to make conversions
+from torch.autograd import Variable
+
